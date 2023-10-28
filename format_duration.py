c163e1cc9be0f85fd3238761df3071b7742d29e1
astronasko <attanasstefanov@gmail.com>
2023-10-28 13:58:04 +0100

    refactor with own solution

--- format_duration.py
+++ format_duration.py
@@ -1,24 +1,36 @@
-def format_duration(seconds):
-    patterns = {5: "{}, {}, {}, {} and {}",
-                4: "{}, {}, {} and {}",
-                3: "{}, {} and {}",
-                2: "{} and {}",
-                1: "{}",
-                0: "now"}
+times = {
+    "year": 365*24*60*60,
+    "day": 24*60*60,
+    "hour": 60*60,
+    "minute": 60,
+    "second": 1
+}
 
-    def format(n: int, time: str):
-        if n:
-            if n == 1:
-                return f"{n} {time}"
-            return f"{n} {time}s"
-        return ""
+def format_duration(t):
+    if t == 0:
+        return "now"
+    
+    output_list = []
 
-    years = format(seconds // 31_536_000, "year")
-    days = format(seconds % 31_536_000 // 86_400, "day")
-    hours = format(seconds % 86_400 // 3600, "hour")
-    minutes = format(seconds % 3600 // 60, "minute")
-    seconds = format(seconds % 60, "second")
+    for k, v in times.items():
+        if t > v:
+            q = t // v
+            
+            if q > 1:
+                output_list.append(
+                    f"{q} {v}s"
+                )
+            else:
+                output_list.append(
+                    f"{q} {v}"
+                )
 
-    not_zero = tuple(filter(lambda x: x != '', (years, days, hours, minutes, seconds)))
-
-    return patterns[len(not_zero)].format(*not_zero)
+            t %= v
+    
+    if len(output_list) > 1:
+        output_string = ", ".join(output_list[:-1])
+        output_string += f"and {output_list[-1]}"
+    else:
+        output_string = output_list[0]
+    
+    return output_string
